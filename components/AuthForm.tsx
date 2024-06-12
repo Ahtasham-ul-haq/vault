4807b177eee5d894d79f8a3c5321c1d2edbe526e
Ahtasham ul-Haq <ahtasham1802@gmail.com>
2024-06-12 22:24:23 +0500

    implementing auth-appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.action";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -47,15 +48,15 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
       // Sign up with appwrite & create plaid token
       if (type === "sign-up") {
-        // const newUser = await SignUp(data)
-        // setUser(newUser)
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
       if (type === "sign-in") {
         // const response = await signIn({
         //   email: data.email,
         //   password: data.password,
-        // })
-        // if(response) router.push('/')
+        // });
+        // if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
