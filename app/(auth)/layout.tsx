664cbb384655057c6cbc8a3c962ebccd035fee9b
Ahtasham ul-Haq <ahtasham1802@gmail.com>
2024-06-15 01:05:58 +0500

    implement authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Vault",
   description: "Vault is amodern banking platform",
   icons: {
     icon: "/icons/logo.svg",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -14,8 +15,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-        {children}
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <Image
+          src="/icons/auth-image.svg"
+          alt="auth-image"
+          width={500}
+          height={500}
+        />
+      </div>
     </main>
   );
 }
